55be32fc93a9743967f809e6aaebeb8c05927380
cirho <me@cirho.com>
2021-09-02 18:45:09 +0200

    completion autoselect

--- lua/config/completion.lua
+++ lua/config/completion.lua
@@ -20,12 +20,7 @@ require('compe').setup {
   documentation = true;
 
   source = {
-    path = true;
-    buffer = true;
     calc = false;
-    vsnip = true;
-    nvim_lsp = true;
-    nvim_lua = true;
     spell = true;
     tags = false;
     snippets_nvim = false;
@@ -45,32 +40,28 @@ local t = function(str)
 end
 
 local check_back_space = function()
-  local col = vim.fn.col('.') - 1
-  if col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-    return true
-  else
-    return false
-  end
+    local col = vim.fn.col('.') - 1
+    return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') ~= nil
 end
 
 _G.tab_complete = function()
   if vim.fn.pumvisible() == 1 then
-    return t '<C-n>'
-  elseif vim.fn.call('vsnip#available', {1}) == 1 then
-    return t '<Plug>(vsnip-expand-or-jump)'
+    return t "<C-n>"
+  elseif vim.fn['vsnip#available'](1) == 1 then
+    return t "<Plug>(vsnip-expand-or-jump)"
   elseif check_back_space() then
-    return t '<Tab>'
+    return t "<Tab>"
   else
     return vim.fn['compe#complete']()
   end
 end
 _G.s_tab_complete = function()
   if vim.fn.pumvisible() == 1 then
-    return t '<C-p>'
-  elseif vim.fn.call('vsnip#jumpable', {-1}) == 1 then
-    return t '<Plug>(vsnip-jump-prev)'
+    return t "<C-p>"
+  elseif vim.fn['vsnip#jumpable'](-1) == 1 then
+    return t "<Plug>(vsnip-jump-prev)"
   else
-    return t '<S-Tab>'
+    return t "<S-Tab>"
   end
 end
 
@@ -81,6 +72,9 @@ utils.keymap('<Tab>', 'v:lua.tab_complete()', 's', opts)
 utils.keymap('<S-Tab>', 'v:lua.s_tab_complete()', 'i', opts)
 utils.keymap('<S-Tab>', 'v:lua.s_tab_complete()', 's', opts)
 
-utils.keymap('<CR>', [[compe#confirm('<CR>')]], 'i', opts)
+-- select first option with space
+utils.keymap('<CR>', [[compe#confirm({ 'keys': '<CR>', 'select': v:true })]], 'i', opts)
+-- invoke completion without typing shit
 utils.keymap('<C-Space>', [[compe#complete()]], 'i', opts)
+-- cancel without exiting insert mode
 utils.keymap('<C-e>', [[compe#close('<C-e>')]], 'i', opts)
