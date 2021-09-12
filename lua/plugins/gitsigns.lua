de617a726a90bc6c927e17649980ea8847db1160
cirho <me@cirho.com>
2021-09-12 20:11:52 +0200

    git: fix text objects and hide popup border

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -15,18 +15,23 @@ require('gitsigns').setup {
     ['n [c'] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns\".prev_hunk()<cr>'"},
 
     ['n <leader>hs'] = '<cmd>lua require"gitsigns".stage_hunk()<cr>',
+    ['n <leader>hS'] = '<cmd>lua require"gitsigns".stage_buffer()<cr>',
     ['n <leader>hu'] = '<cmd>lua require"gitsigns".undo_stage_hunk()<cr>',
     ['n <leader>hr'] = '<cmd>lua require"gitsigns".reset_hunk()<cr>',
     ['n <leader>hp'] = '<cmd>lua require"gitsigns".preview_hunk()<cr>',
     ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line()<cr>',
 
     -- text objects
-    ['o ih'] = ':<C-U>lua require"gitsigns".text_object()<cr>',
-    ['x ih'] = ':<C-U>lua require"gitsigns".text_object()<cr>'
+    ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<cr>',
+    ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<cr>'
   },
   watch_index = {
-    interval = 1000
+    interval = 1000,
+    follow_files = true
   },
   sign_priority = 6,
   status_formatter = nil,
+  preview_config = {
+    border = 'none'
+  },
 }
