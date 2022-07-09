5508b6c7ae62fd46b81eb4f2982faba948659520
cirho <me@cirho.com>
2022-07-09 23:56:11 +0200

    snippets: simplify the config by using a new loader of luasnip

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,17 +1,3 @@
-local ls = require("luasnip")
-local scan = require('plenary.scandir')
-
 local root_path = os.getenv('XDG_CONFIG_HOME') .. '/nvim/snippets/'
-local files = scan.scan_dir(root_path, { search_pattern = '.lua' })
 
-local snips = {}
-for _, path in ipairs(files) do
-  local p,q = path:find('%w*%.lua')
-  local ft = path:sub(p, q - 4)
-
-  snips[ft] = loadfile(path)()
-end
-
-ls.snippets = vim.tbl_extend('force', snips, {
-  all = {},
-})
+require("luasnip.loaders.from_lua").load({paths = root_path})
