5a6886b4f7a3d840b563634610bf1133c7250dbb
cirho <me@cirho.com>
2023-10-03 20:57:17 +0200

    plugins: luasnip: standardize the setup method

--- init.lua
+++ init.lua
@@ -16,22 +16,9 @@ vim.loader.enable()
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 vim.opt.rtp:prepend(lazypath)
 
-local use_conf = function(repo, deps)
-  local name = repo:match('/([%w-]*)'):gsub('.*-', '')
-  local deps = deps or {}
-
-  return {
-    repo,
-    dependencies = deps,
-    config = function()
-      require('plugins.' .. name);
-    end
-  }
-end
-
 require("lazy").setup({
   'neovim/nvim-lspconfig',
-  use_conf('L3MON4D3/luasnip'),
+  require('plugins.luasnip'),
   require('plugins.cmp'),
   require('plugins.telescope'),
   require('plugins.gitsigns'),
--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,3 +1,8 @@
-local root_path = os.getenv('XDG_CONFIG_HOME') .. '/nvim/snippets/'
-
-require("luasnip.loaders.from_lua").load({paths = root_path})
+return {
+  'neovim/nvim-lspconfig',
+  config = function()
+    require("luasnip.loaders.from_lua").load({
+      paths = os.getenv('XDG_CONFIG_HOME') .. '/nvim/snippets/'
+    })
+  end
+}
