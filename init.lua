b3de33b2b2027f39f742bcc65564f78821f5cd39
cirho <me@cirho.com>
2023-10-26 10:21:53 +0200

    lang: asm: prefer at&t syntax over intel one

--- init.lua
+++ init.lua
@@ -7,8 +7,6 @@ vim.g.loaded_node_provider = 0
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
 
-vim.g.asmsyntax = 'nasm'
-
 require('lo.debug')
 require('lo.options')
 require('lo.diagnostics')
