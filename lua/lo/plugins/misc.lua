7cb6999d880e7ca7387bb8fb0ac91571f6f54899
cirho <me@cirho.com>
2023-11-05 15:59:15 +0100

    looks: switch theme to gruvbox

--- lua/lo/plugins/misc.lua
+++ lua/lo/plugins/misc.lua
@@ -5,10 +5,37 @@ return {
   }, {
     'nvim-lua/plenary.nvim',
   }, {
-    'sainnhe/sonokai',
+    'ellisonleao/gruvbox.nvim',
     config = function()
-      vim.g.sonokai_style = 'shusia'
-      vim.cmd [[ colorscheme sonokai ]]
+      require("gruvbox").setup({
+        terminal_colors = true,
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = false,
+          emphasis = false,
+          comments = false,
+          operators = false,
+          folds = false,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true,
+        contrast = "",
+        palette_overrides = {
+          grey = '#ffffff'
+        },
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      })
+
+      vim.o.background = "dark"
+      vim.cmd([[colorscheme gruvbox]])
     end
   }
 }
